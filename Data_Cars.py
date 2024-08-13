d52ebb100e05f0f3a958d50aca1fdce2eb54d667
Oscar <oocamargo@unbosque.edu.co>
2024-08-12 21:46:06 -0500

    Hoy dia 12-08-2024 Se agregaaron algunas graficas

--- Data_Cars.py
+++ Data_Cars.py
@@ -5,7 +5,8 @@ import matplotlib as mpl
 import seaborn as sns
 
 # Se creó la ruta del archivo 
-Ruta = 'C:/Users/oscar/OneDrive/Escritorio/CODER/Data Science I Fundamentos para la Ciencia de Datos/Data_science_Coder_Oscar_Camargo/BASES_DE_DATOS'
+Ruta = 'C:/Users/oscar/OneDrive/Escritorio/CODER/Data Science I Fundamentos para la Ciencia de Datos/Data_science_Coder_Oscar_Camargo/Data_science_Coder_Oscar_Camargo/BASES_DE_DATOS'
+
 Datos_de_autos = Ruta + "/Cars_database.xlsx"
 
 #Creación del Dataframe 
@@ -101,4 +102,53 @@ plt.title('Relación entre Año y Precio de los Autos por Marca')
 plt.xlabel('Año')
 plt.ylabel('Precio')
 plt.grid(True)
+plt.show()
+
+# Distribución del Precio por Año:
+
+plt.figure(figsize=(12, 6))
+sns.boxplot(x='year', y='price', data=df_cars)
+plt.title('Distribución del Precio por Año')
+plt.xlabel('Año')
+plt.ylabel('Precio')
+plt.xticks(rotation=45)
+plt.show()
+
+# Precio promedio por año 
+
+plt.figure(figsize=(14, 8))
+sns.barplot(x='Make', y='price', data=df_cars, estimator=lambda x: sum(x) / len(x))
+plt.title('Precio Promedio por Fabricante')
+plt.xlabel('Fabricante')
+plt.ylabel('Precio Promedio')
+plt.xticks(rotation=45)
+plt.show()
+
+#Relación entre Año y Precio
+
+plt.figure(figsize=(12, 6))
+sns.scatterplot(x='year', y='price', hue='Make', data=df_cars)
+sns.lineplot(x='year', y='price', hue='Make', data=df_cars, ci=None, estimator='mean', marker='o')
+plt.title('Relación entre Año y Precio por Fabricante')
+plt.xlabel('Año')
+plt.ylabel('Precio')
+plt.xticks(rotation=45)
+plt.show()
+
+#  Tendencias de Precio a lo Largo del Tiempo por Fabricante:
+
+plt.figure(figsize=(14, 8))
+sns.lineplot(x='year', y='price', hue='Make', data=df_cars, marker='o')
+plt.title('Tendencia de Precios a lo Largo del Tiempo por Fabricante')
+plt.xlabel('Año')
+plt.ylabel('Precio')
+plt.xticks(rotation=45)
+plt.show()
+
+#Heatmap de Correlación entre Año y Precio:
+
+corr_matrix = df_cars[['year', 'price']].corr()
+plt.figure(figsize=(8, 6))
+sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt=".2f")
+plt.title('Heatmap de Correlación entre Año y Precio')
 plt.show()
